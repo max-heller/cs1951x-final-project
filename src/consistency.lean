cf6d0477c9d0650c26952d15ac559acad7b10b07
Max Heller <max_heller@brown.edu>
2021-12-21 10:53:54 -0500

    consistent_extensible

--- src/consistency.lean
+++ src/consistency.lean
@@ -4,21 +4,22 @@ import .derivations
 /-- A set `s` is `Σ`-consistent if it does not `Σ`-derive `⊥`.  -/
 def set.consistent (s axms : set formula) : Prop := ¬(s ⊢[axms] ⊥)
 
-lemma consistent_iff_any_nonderivable (Γ : set formula) (axms) :
-  Γ.consistent axms ↔ ∃a, ¬(Γ ⊢[axms] a) :=
-sorry
-
-lemma derivable_iff_not_inconsistent (Γ : set formula) (axms) (a) :
-  (Γ ⊢[axms] a) ↔ ¬Γ.consistent (axms ∪ {¬a}) :=
-sorry
-
 lemma consistent_extensible (Γ : set formula) (axms) :
   Γ.consistent axms → ∀a, (Γ ∪ {a}).consistent axms ∨ (Γ ∪ {¬a}).consistent axms :=
 begin
   contrapose,
   simp only [not_forall],
   intro h,
-  apply exists.elim h,
-  intros a ha,
-  sorry
+  cases h with a ha,
+  simp [set.consistent, -derivable.from] at ha,
+  rw ←and_iff_not_or_not at ha,
+  cases ha with ha hna,
+  rw [set.consistent, not_not],
+  rw [←set.union_singleton, derivable.deduction] at *,
+  apply derivable.from.mp (a ⟶ ⊥) _ _ ha,
+  apply derivable.from.mp (¬a ⟶ ⊥) _ _ hna,
+  apply derivable.monotonicity ∅,
+  { simp, },
+  { rw derivable.from.no_premises,
+    derive_taut, },
 end
